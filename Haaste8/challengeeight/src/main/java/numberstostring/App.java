f0e16d78020f34a3caec53efebfc5fdf872440ea
SJarno <jarno.saastamoinen@hotmail.com>
2021-08-30 08:55:00 +0300

    minor edits

--- Haaste8/challengeeight/src/main/java/numberstostring/App.java
+++ Haaste8/challengeeight/src/main/java/numberstostring/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
 
         StringParser sParser = new StringParser();
-        sParser.readInput(" ");
+        sParser.readInput("Seitsemän on 7");
         sParser.parseText();
         System.out.println(sParser.getOriginalText());
         System.out.println(sParser.getParsedText());
